fc9ae123337e1bcd4512759f29a2c5ec24f208ef
niranjan1988 <valliveduniranjan@gmail.com>
2021-03-15 10:00:29 +0530

    Mongoose $inc update operator

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Update a User', () => {
     let joe;
 
     beforeEach((done) => {
-        joe = new User({ name: 'Joe' });
+        joe = new User({ name: 'Joe', postCount: 0 });
         joe.save().then(() => {
             done();
         })
@@ -38,5 +38,16 @@ describe('Update a User', () => {
 
     it('Class based update by _id', (done) => {
         assertName(User.findByIdAndUpdate(joe._id,{name:'Alex'}),done);
+    });
+
+    //====================================================================================================
+
+    it('Increment the postCount of  users by 1 on the Mongodb side', (done) => {
+        User.updateMany({name:'Joe'},{$inc:{postCount:1}})
+        .then(()=> User.findOne({name:'Joe'}))
+        .then((user)=> {
+            assert(user.postCount == 1);
+            done();
+        })
     });
 });
