54e04e1732b2d12c3b3007633cebc45f6288b551
niranjan1988 <valliveduniranjan@gmail.com>
2021-03-30 13:03:12 +0530

    Virtual types

--- test/update_test.js
+++ test/update_test.js
@@ -5,13 +5,13 @@ describe('Update a User', () => {
     let joe;
 
     beforeEach((done) => {
-        joe = new User({ name: 'Joe' });
+        joe = new User({ name: 'Joe', likes: 0 });
         joe.save().then(() => {
             done();
         })
     });
 
-    function assertName(operation,done) {
+    function assertName(operation, done) {
         operation.then(() => User.findOne({ name: 'Alex' }))
             .then(users => {
                 assert(users.name == 'Alex');
@@ -21,33 +21,33 @@ describe('Update a User', () => {
 
     it('Instance type using set and save', (done) => {
         joe.set('name', 'Alex');
-        assertName(joe.save(),done);
+        assertName(joe.save(), done);
     });
 
     it('Model Instance can update', (done) => {
-        assertName(joe.update({ name: 'Alex' }),done);
+        assertName(joe.update({ name: 'Alex' }), done);
     });
 
     it('Class based update', (done) => {
-        assertName(User.updateOne({ name: 'Joe' },{name:'Alex'}),done);
+        assertName(User.updateOne({ name: 'Joe' }, { name: 'Alex' }), done);
     });
 
     it('Class based update', (done) => {
-        assertName(User.findOneAndUpdate({ name: 'Joe' },{name:'Alex'}),done);
+        assertName(User.findOneAndUpdate({ name: 'Joe' }, { name: 'Alex' }), done);
     });
 
     it('Class based update by _id', (done) => {
-        assertName(User.findByIdAndUpdate(joe._id,{name:'Alex'}),done);
+        assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex' }), done);
     });
 
     //====================================================================================================
 
-    xit('Increment the postCount of  users by 1 on the Mongodb side', (done) => {
-        User.updateMany({name:'Joe'},{$inc:{postCount:1}})
-        .then(()=> User.findOne({name:'Joe'}))
-        .then((user)=> {
-            assert(user.postCount == 1);
-            done();
-        })
+    it('Increment the likes of  users by 1 on the Mongodb side', (done) => {
+        User.updateMany({ name: 'Joe' }, { $inc: { likes: 1 } })
+            .then(() => User.findOne({ name: 'Joe' }))
+            .then((user) => {
+                assert(user.likes == 1);
+                done();
+            })
     });
 });
