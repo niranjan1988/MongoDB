4c4625f627347c73a8b4419059ed86a42c174526
niranjan1988 <valliveduniranjan@gmail.com>
2021-04-03 14:33:32 +0530

    Slip, limit and sort

--- test/reading_test.js
+++ test/reading_test.js
@@ -2,10 +2,13 @@ const User = require("../src/user")
 const assert = require('assert');
 
 describe("Reading users from database", () => {
-    let joe;
+    let joe, maria, alex, zach;
     beforeEach((done) => {
         joe = new User({ name: 'Joe' });
-        joe.save().then(() => {
+        maria = new User({ name: 'Maria' });
+        alex = new User({ name: 'Alex' });
+        zach = new User({ name: 'Zach' });
+        Promise.all([joe.save(), maria.save(), alex.save(), zach.save()]).then(() => {
             done();
         });
     })
@@ -14,7 +17,7 @@ describe("Reading users from database", () => {
         User.find({ name: 'Joe' }).then((users) => {
             assert(users[0]._id.toString() == joe._id);
             done();
-            
+
         })
     })
 
@@ -22,7 +25,19 @@ describe("Reading users from database", () => {
         User.findOne({ _id: joe._id }).then((users) => {
             assert(users.name == joe.name);
             done();
-            
+
         })
     })
+
+    it('it can skip and limit the result set', (done) => {
+        User.find({})
+        .sort({name:1})  // 1 means assending order, -1 means decending order
+        .skip(3)
+        .limit(5)
+        .then(users => {
+            console.log(users);
+            assert(users.length == 1);
+            done();
+        })
+    });
 })
